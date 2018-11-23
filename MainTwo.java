4fc5c67096af9b0ecbef4ccef1fe7a6a37037af9
IOlesea <43409107+IOlesea@users.noreply.github.com>
2018-11-23 16:20:07 +0200

    home 23.11

---
+++ MainTwo.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static com.company.Random1.*;
+
+public class MainTwo {
+    public static void main(String... args) {
+
+
+
+        PrintArray.PrintA();
+        PrintArray.PrintB();
+        PrintArray.PrintD();
+        PrintArray.PrintF();
+        PrintArray.PrintE1();
+        PrintArray.PrintG();
+        PrintArray.PrintI();
+        PrintArray.PrintJ();
+        // ?PrintArray.PrintF1();
+        //?PrintArray.PrintE();
+        //??? PrintArray.PrintC();
+    }
+}
